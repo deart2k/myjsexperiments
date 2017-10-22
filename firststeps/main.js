053f528a9634d54ac966306aa28fe64ae2238d67
deart <deart@list.ru>
2017-10-22 23:37:13 +0300

    firststeps

--- firststeps/main.js
+++ firststeps/main.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
-    'use strict';
-    paper.install(window);
-    paper.setup(document.getElementById('mainCanvas'));
-
-    var c;
-    for(var x=25;x<400; x+=50) {
-        for (var y = 25; y < 400; y += 50) {
-            c = Shape.Circle(x, y, 20);
-            c.fillColor = 'green';
-        }
-    }
+   'use strict';
+   paper.install(window);
+   paper.setup(document.getElementById('mainCanvas'));
+   var c = Shape.Circle(200,200,80);
+   c.fillColor = "pink";
+   var text = new  PointText (200,200);
+   text.justification = 'center';
+   text.fillColor = 'green';
+   text.fontSize = 20;
+   text.content = 'Алиса не дура';
+   var tool = new Tool();
+   tool.onMouseDown = function (even) {
+       var c = Shape.Circle(even.point.x,even.point.y,20);
+       c.fillColor = 'yellow';
+   }
     paper.view.draw();
     console.log('main.js loaded');
 });
