4cea8768ae3e2951be5683f9a27426d8b50ddcd3
deart <deart@list.ru>
2017-10-22 23:10:29 +0300

    firststeps

--- firststeps/main.js
+++ firststeps/main.js
@@ -4,9 +4,11 @@ $(document).ready(function () {
     paper.setup(document.getElementById('mainCanvas'));
 
     var c;
-    for(var x=25;x<400; x+=50) for (var y = 25; y < 400; y += 50) {
-        c = Shape.Circle(x, y, 20);
-        c.fillColor = 'green';
+    for(var x=25;x<400; x+=50) {
+        for (var y = 25; y < 400; y += 50) {
+            c = Shape.Circle(x, y, 20);
+            c.fillColor = 'green';
+        }
     }
     paper.view.draw();
     console.log('main.js loaded');
